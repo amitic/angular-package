b6f4781297b719fc834f531974bf1479a2ec1eda
angularpackage <angular-package@wwwdev.io>
2018-05-29 17:10:48 +0200

    refactor(PropertyClass): Change class name, add some properties and methods, use tslint rules.
    - Changed class from `PropertyWrapperClass` to `PropertyClass`.
    - Added `binded` property to store binded properties.
    - Added `bind()` method to bind component properties to target.
    - Added `define()` method to defineProperties with javascript in source.
    - Changed method name `wrapProperty` to `_wrap`.
    - Changed typescript definitions.
    - Added `_bind()` method to bind one property.
    - Added `_clear()` method to remove from binded and remove setter and getter in one property.

--- packages/core/packages/property-wrapper/src/property-wrapper.class.ts
+++ packages/core/packages/property-wrapper/src/property-wrapper.class.ts
@@ -1,46 +1,109 @@
 import { StoreOriginalClass } from '../../store';
 import { PrefixSuffixClass } from '../../src/prefixsuffix.class';
-import { CallbackGetterType } from '../type/callback-getter.type';
-import { CallbackSetterType } from '../type/callback-setter.type';
+import { Getter } from '../type/getter.type';
+import { Setter } from '../type/setter.type';
 
 /**
  * Wrap indicated properties of source component with instance dynamic component.
  * @export
- * @class PropertyWrapperClass
+ * @class PropertyClass
  */
-export class PropertyWrapperClass extends PrefixSuffixClass {
+export class PropertyClass extends PrefixSuffixClass {
 
+  /**
+   * Instance of store.
+   * @private
+   * @type {StoreOriginalClass}
+   * @memberof PropertyClass
+   */
   private store: StoreOriginalClass = new StoreOriginalClass();
+
+  /**
+   * List of properties that were being binded.
+   * @private
+   * @type {string[]}
+   * @memberof PropertyClass
+   */
+  private _binded: string[] = [];
+  set binded(property: string | string[] | number) {
+    if (property instanceof Array) {
+      property.forEach(p => {
+        this._binded.push(p);
+      })
+    }
+    if (typeof property === 'string') {
+      this._binded.push(property);
+    }
+    if (typeof property === 'number') {
+      this._binded.splice(property, 1);
+    }
+  }
+  get binded(): string | string[] | number {
+    return this._binded;
+  }
+
+  /**
+   * List of properties that were being wrapped.
+   * @private
+   * @type {string[]}
+   * @memberof PropertyClass
+   */
   private wrapped: string[] = [];
 
   /**
-   * Creates an instance of PropertyWrapperClass.
+   * Creates an instance of PropertyClass.
    * @param {string} [prefix]
    * @param {string} [suffix]
-   * @memberof PropertyWrapperClass
+   * @memberof PropertyClass
    */
   constructor(prefix?: string, suffix?: string) {
     super(prefix, suffix);
   }
 
   /**
-   * Method to wrap specified properties with setter and getter callback method.
-   * @template S
-   * @param {(Function | S)} source Function or component.
-   * @param {(string | string[])} properties Name of properties that will be wrapped.
-   * @param {CallbackSetterType<S>} setter Callback function used on set.
-   * @param {CallbackGetterType<S>} getter Callback function used on get.
-   * @memberof PropertyWrapperClass
+   * @template S 
+   * @template T 
+   * @param {(Function | S)} source 
+   * @param {(string | string[])} properties 
+   * @param {T} targetName 
+   * @memberof PropertyClass
    */
-  wrap<S>(source: Function | S, properties: string | string[], setter: CallbackSetterType<S>, getter: CallbackGetterType<S>): void {
+  bind<S, T = string>(source: Function | S, properties: string | string[], targetName: T): void {
     try {
       if (source) {
         if (properties instanceof Array) {
           properties.forEach((property: string) => {
-            this.wrapProperty(source, property, setter, getter);
+            if (typeof targetName === 'string') {
+              this._bind(source, property, targetName);
+            }
           });
-        } else {
-          this.wrapProperty(source, properties, setter, getter);
+        } else if (typeof targetName === 'string') {
+          this._bind(source, properties, targetName);
+        }
+      }
+    } catch (e) {
+      if (e instanceof TypeError) {
+      } else if (e instanceof RangeError) {
+      } else if (e instanceof EvalError) {
+      } else {
+      }
+      console.warn(e.message);
+    } finally { }
+  }
+
+  /**
+   * @template S 
+   * @template T 
+   * @param {(Function | S)} source 
+   * @param {(string | string[])} properties 
+   * @memberof PropertyClass
+   */
+  clear<S>(source: Function | S, properties?: string | string[]): void {
+    try {
+      if (source) {
+        const p = (properties) ? (typeof properties === 'string') ? [properties] : properties : this.binded;
+        if (p instanceof Array) {
+          p.forEach((property: string) => this._clear(source, property));
         }
       }
     } catch (e) {
@@ -53,10 +116,141 @@ export class PropertyWrapperClass extends PrefixSuffixClass {
     }
   }
 
-  private wrapProperty<S>(source: Function | S, property: string, setter: CallbackSetterType<S>, getter: CallbackGetterType<S>): void {
-    // Check if property is already wrapped.
+  /**
+   * @template S 
+   * @template R 
+   * @param {(Function | S)} source 
+   * @param {string} property 
+   * @param {ThisType<any>} descriptor 
+   * @memberof PropertyClass
+   */
+  define<S>(source: Function | S, property: string, descriptor: ThisType<any>): void {
+    Object.defineProperties(
+      (source instanceof Function) ? source.prototype : source, {
+        [property]: descriptor
+      }
+    );
+  }
+
+  /**
+   * Method to wrap specified properties with setter and getter callback method.
+   * @template S Source type.
+   * @template R Result type of `setter()` and `getter()` function.
+   * @param {(Function | S)} source Function or component.
+   * @param {(string | string[])} properties Name of properties that will be wrapped.
+   * @param {Setter<S, R>} setter Callback function used on set.
+   * @param {Getter<S, R>} getter Callback function used on get.
+   * @memberof PropertyClass
+   */
+  wrap<S, R = any>(source: Function | S, properties: string | string[], setter: Setter<S, R>, getter: Getter<S, R>): void {
+    try {
+      if (source) {
+        if (properties instanceof Array) {
+          properties.forEach((property: string) => {
+            this._wrap(source, property, setter, getter);
+          });
+        } else {
+          this._wrap(source, properties, setter, getter);
+        }
+      }
+    } catch (e) {
+      if (e instanceof TypeError) {
+      } else if (e instanceof RangeError) {
+      } else if (e instanceof EvalError) {
+      } else {
+      }
+      console.warn(e.message);
+    }
+  }
+
+  /**
+   * Bind property from component one to one to service in the same component with targetName.
+   * @private
+   * @template S 
+   * @param {(Function | S)} source 
+   * @param {string} property
+   * @param {string} target Name of the object (for example service) in the same component that property will be binded to.
+   * @memberof PropertyClass
+   */
+  private _bind<S, T = string, R = any>(source: Function | S, property: string, target: T): void {
+    // Check if property is already used.
+    if (this.binded instanceof Array && this.binded.includes(property) === false) {
+
+      // Store original Setter/Getter.
+      const store = this.store.setterGetter(source, property);
+
+      // Bind property to source.
+      Object.defineProperties((source instanceof Function) ? source.prototype : source, {
+        [property]: {
+          get: function (): R {
+            // Use old getter.
+            if (store.getter[property]) {
+              store.getter[property].apply(this, arguments);
+            }
+            if (typeof target === 'string') {
+              return this[target][property];
+            } else {
+              return target[property];
+            }
+          },
+          set: function (value: R) {
+            // Use old setter.
+            if (store.setter[property]) { 
+              store.setter[property].apply(this, arguments);
+            }
+            if (typeof target === 'string') {
+              this[target][property] = value;
+            } else {
+              target[property] = value;
+            }
+          }
+        }
+      });
+
+      // Property is used.
+      this.binded = property;
+    }
+  }
+
+  /**
+   * @private
+   * @template S 
+   * @template T 
+   * @template R 
+   * @param {(Function | S)} source 
+   * @param {string} property 
+   * @memberof PropertyClass
+   */
+  private _clear<S>(source: Function | S, property: string): void {
+    // Check if property is already used.
+    if (this.binded instanceof Array && this.binded.includes(property) === true) {
+
+      this.define(source, property, {
+        get: undefined,
+        set: undefined
+      });
+
+      // Property is removed from used.
+      const index: number = this.binded.indexOf(property);
+      if (index > -1) {
+        this.binded = index;
+      }
+    }
+  }
+
+  /**
+   * @private
+   * @template S 
+   * @template R 
+   * @param {(Function | S)} source 
+   * @param {string} property 
+   * @param {Setter<S, R>} setter 
+   * @param {Getter<S, R>} getter 
+   * @memberof PropertyClass
+   */
+  private _wrap<S, R = any>(source: Function | S, property: string, setter: Setter<S, R>, getter: Getter<S, R>): void {
+    // Check if property is already used.
     if (this.wrapped.includes(property) === false) {
-      const t = this;
 
       // Store original Setter/Getter.
       const store = this.store.setterGetter(source, property);
@@ -66,16 +260,18 @@ export class PropertyWrapperClass extends PrefixSuffixClass {
 
       // Wrap property.
       if (sourcePropertyName) {
+
         // Create property with prefix and suffix to be wrapped by original name.
+        // this.define(source, sourcePropertyName, )
         Object.defineProperty(
           (source instanceof Function) ? source.prototype : source,
           sourcePropertyName,
           { writable: true, value: (source[property]) ? source[property] : source[sourcePropertyName] }
-        );
+        );  
 
         Object.defineProperties((source instanceof Function) ? source.prototype : source, {
           [property]: {
-            get: function () {
+            get: function (): R | undefined {
               if (store.getter[property]) {
                 return store.getter[property].apply(this, arguments);
               }
@@ -85,7 +281,7 @@ export class PropertyWrapperClass extends PrefixSuffixClass {
                 return getter(property, this) || this[sourcePropertyName];
               }
             },
-            set: function (value) {
+            set: function (value: R | undefined) {
               // Remember input value.
               this[sourcePropertyName] = value;
 
