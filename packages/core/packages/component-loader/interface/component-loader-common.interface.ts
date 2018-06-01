0a6635f44c08b76195d7bd16a43e377bbf5f25d9
angularpackage <angular-package@wwwdev.io>
2018-06-01 17:31:37 +0200

    refactor(ComponentLoaderCommonInterface): Use new tslint rules.
    Used tslint rules:
    - Use a method signature instead of a property signature of function type. (prefer-method-signature)
    - Array type using 'T[]' is forbidden. Use 'Array<T>' instead. (array-type)
    - Import 'ComponentFactoryResolver' is unused. (no-unused)

--- packages/core/packages/component-loader/interface/component-loader-common.interface.ts
+++ packages/core/packages/component-loader/interface/component-loader-common.interface.ts
@@ -1,23 +1,25 @@
-import { ComponentFactoryResolver, ComponentRef, Type } from '@angular/core';
+import {
+  ComponentRef,
+  Type
+} from '@angular/core';
 
 /**
  * @export
- * @interface ComponentLoaderCommonInterface
- * @template T
+ * @template T Type of component to load.
  */
 export interface ComponentLoaderCommonInterface<T> {
 
   __component?: ComponentRef<T>;
   __componentPropertyName?: string;
-  __create?: <D = T>(component: Type<D>) => this;
-  __destroy?: () => any;
-  __properties?: string[];
+  __create?: ((component: Type<T>) => this);
+  __destroy?: (() => any);
+  __properties?: Array<string>;
   __prefix?: string;
   __suffix?: string;
 
-  __assign?: <S>(p: string | string[], source: S) => void;
-  __set?: <PT>(property: string, value: PT) => void;
-  __get?: <PT>(property: string) => PT | undefined;
-  __subscribe?: (property: string, ...args: any[]) => void;
+  __assign?: (<S>(p: string | Array<string>, source: S) => void);
+  __set?: (<PT>(property: string, value: PT) => void);
+  __get?: (<PT>(property: string) => PT | undefined);
+  __subscribe?: ((property: string, ...args: Array<any>) => void);
 
 }
